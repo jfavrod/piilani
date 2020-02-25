54f464540bc5fd3deaf8047b92f3a7a569aa6023
jfavrod <jason [at] jasonfavrod.com>
2020-02-25 13:51:14 -1000

    Updated context/interfaces
    IDatabaseVals: enabling users to provide ssl/tls configuration details.
    
    IDatabaseSSL: added interface which provides the structure for adding
    the ssl/tls details to IDatabaseVals objects.
    
    IConfig: Added getConfigDir method for retrieving the abs path to the
    configuration directory. Added getDatabaseConfig method for retrieving
    the database configuration.

--- src/context/interfaces.ts
+++ src/context/interfaces.ts
@@ -1,11 +1,14 @@
 import { DBDriver, Env } from './enums';
 
 export interface IConfig {
+    /** Get absolute path to the config directory. */
+    getConfigDir(): string;
     /**
      * Get a database connection string.
      * @param db If multiple databases configured, use db to select.
      */
     getConnectionString(db?: string): string;
+    getDatabaseConfig(): IDatabaseVals | IMultiDatabaseVals | undefined;
     getEnv(): Env;
     getListenPort(): number | undefined;
     getServiceConfig(serviceName: string): {[key: string]: any} | undefined;
@@ -20,12 +23,22 @@ export interface IConfigValues {
     services?: { [key: string]: IService };
 }
 
+export interface IDatabaseSSL {
+    /** Path, relative to config directory, where ca file is located. */
+    ca?: string;
+    /** Path, relative to config directory, where cert file is located. */
+    cert?: string;
+    /** Path, relative to config directory, where key file is located. */
+    key?: string;
+}
+
 export interface IDatabaseVals {
     driver: DBDriver;
     host: string;
     name: string;
     password: string;
     port?: number;
+    ssl?: IDatabaseSSL;
     user: string;
 }
 
