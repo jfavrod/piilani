31c50fac5581fcf937bcb7acdb949b4811ed5e3f
Jason Favrod <jason.favrod@monsanto.com>
2020-05-18 18:04:02 -1000

    Added sendGatewayResponse Method to ExpressService
    The method sends IGatewayResponseObjects as Express Responses.

--- src/services/baseClasses/ExpressService.ts
+++ src/services/baseClasses/ExpressService.ts
@@ -5,6 +5,7 @@
 
 import { Response } from 'express';
 import { ILogger } from '../../context/interfaces';
+import { IGatewayResponse } from '../../gateways/interfaces';
 import { IServiceResponse } from '../interfaces';
 import ServiceBase from './ServiceBase';
 
@@ -25,7 +26,48 @@ export default abstract class ExpressService extends ServiceBase {
         } as IServiceResponse);
     }
 
-    protected sendServiceResponse = (res: Response, serviceResponse: IServiceResponse) => {
+    /**
+     * @param res An express Response object.
+     * @param dbResponse The response from a Table Data Gateway used by this Service.
+     * @param created Set to true if the request was for creating a database record.
+     */
+    protected sendGatewayResponse(res: Response, dbResponse: IGatewayResponse, created?: boolean) {
+        const response: IServiceResponse = {
+            code: 200,
+            data: undefined,
+            error: false,
+            message: undefined,
+        };
+
+        if (dbResponse.class === 0) {
+            if (created) {
+                response.code = 201;
+            }
+            if (dbResponse.data && dbResponse.data instanceof Array && !dbResponse.data.length) {
+                response.code = 204;
+            }
+        }
+        else if (dbResponse.class === 1) {
+            response.code = 400;
+            response.error = true;
+        }
+        else {
+            response.code = 500;
+            response.error = true;
+        }
+
+        if (dbResponse.data) {
+            response.data = dbResponse.data;
+        }
+
+        if (dbResponse.message) {
+            response.message = dbResponse.message;
+        }
+
+        res.status(response.code).json(response);
+    }
+
+    protected sendServiceResponse(res: Response, serviceResponse: IServiceResponse) {
         res.status(serviceResponse.code).json(serviceResponse);
     }
 }
