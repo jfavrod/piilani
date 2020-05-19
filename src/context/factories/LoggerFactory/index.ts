2a120c6f045e3e130d5f79043388f729cd8ba121
Jason Favrod <jason.favrod@monsanto.com>
2020-05-18 17:57:40 -1000

    Updated LoggerFactory
    To return a new ILogger instance based winston logger.

--- src/context/factories/LoggerFactory/index.ts
+++ src/context/factories/LoggerFactory/index.ts
@@ -3,7 +3,12 @@
  * @module Context.Factories
  */
 
+import { createLogger, format, transports } from 'winston';
+import * as Transport from 'winston-transport';
+import { IConfig } from '../../Config/interfaces';
 import { ILogger } from '../../interfaces';
+
+import ConfigFactory from '../ConfigFactory';
 
 /**
  * Instantiates ILogger objects.
@@ -16,7 +21,36 @@ import { ILogger } from '../../interfaces';
  * ```
  */
 export default class LoggerFactory {
+    private static config: IConfig | undefined;
+    private static logger: ILogger | undefined;
+
     public static getInstance(): ILogger {
-        return console;
+        if (!LoggerFactory.config) {
+            LoggerFactory.config = ConfigFactory.getInstance();
+        }
+
+        if (!LoggerFactory.logger) {
+            const loggingConfig = LoggerFactory.config.getLoggingConfig()!;
+            loggingConfig!.format = format.json();
+
+            loggingConfig.transports = [] as Transport[];
+
+            if (!loggingConfig?.noConsole) {
+                loggingConfig.transports.push(new transports.Console());
+            }
+
+            if (loggingConfig.http) {
+                loggingConfig.transports.push(
+                    new transports.Http({
+                        host: loggingConfig.http.host,
+                        port: loggingConfig.http.port,
+                    }),
+                );
+            }
+
+            LoggerFactory.logger = createLogger(loggingConfig);
+        }
+
+        return LoggerFactory.logger!;
     }
 }
