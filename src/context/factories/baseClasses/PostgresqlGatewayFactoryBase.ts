01a9cb6da04f6de0f143b1dbc044d4b0f9001483
Jason Favrod <jason.favrod@monsanto.com>
2020-05-18 17:59:01 -1000

    Updated PostgresqlGatewayFactoryBase
    Now using prototype methods instead of array methods.

--- src/context/factories/baseClasses/PostgresqlGatewayFactoryBase.ts
+++ src/context/factories/baseClasses/PostgresqlGatewayFactoryBase.ts
@@ -19,27 +19,58 @@ import FactoryBase from './FactoryBase';
  * }
  */
 export default class PostgresqlGatewayFactoryBase extends FactoryBase {
-    protected getPool = (): Pool => {
-        let dbConfig = this.config.getDatabaseConfig();
-        let caFile: string | undefined;
+    private pool: Pool | undefined;
 
-        if (!(dbConfig instanceof Array)) {
-            dbConfig = dbConfig as IDatabaseVals;
-            if (dbConfig?.ssl && dbConfig.ssl!.ca) {
-                caFile = (dbConfig as IDatabaseVals).ssl!.ca!;
+    constructor() {
+        super();
+        this.disconnect = this.disconnect.bind(this);
+        this.getPool = this.getPool.bind(this);
+    }
+
+    /**
+     * End the Pool connection to the database.
+     * Subsequent calls that return gateways should establish a new
+     * connection Pool.
+     */
+    public async disconnect() {
+        if (this.pool) {
+            try {
+                await this.pool.end();
+                this.pool = undefined;
+            }
+            catch {
+                return false;
             }
         }
 
-        if (caFile) {
-            return new Pool({
-                connectionString: this.config.getConnectionString(),
-                ssl: {
-                    ca: fs.readFileSync(`${this.config.getConfigDir()}/${caFile}`),
-                    rejectUnauthorized: false,
-                },
-            });
+        return true;
+    }
+
+    protected getPool(): Pool {
+        if (!this.pool) {
+            let dbConfig = this.config.getDatabaseConfig();
+            let caFile: string | undefined;
+
+            if (!(dbConfig instanceof Array)) {
+                dbConfig = dbConfig as IDatabaseVals;
+                if (dbConfig?.ssl && dbConfig.ssl!.ca) {
+                    caFile = (dbConfig as IDatabaseVals).ssl!.ca!;
+                }
+            }
+
+            if (caFile) {
+                return new Pool({
+                    connectionString: this.config.getConnectionString(),
+                    ssl: {
+                        ca: fs.readFileSync(`${this.config.getConfigDir()}/${caFile}`),
+                        rejectUnauthorized: false,
+                    },
+                });
+            }
+
+            this.pool = new Pool({ connectionString: this.config.getConnectionString() });
         }
 
-        return new Pool({ connectionString: this.config.getConnectionString() });
+        return this.pool;
     }
 }
