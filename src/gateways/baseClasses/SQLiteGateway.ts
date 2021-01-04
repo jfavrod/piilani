463bf1d07bd4ba48aa0563ce980792dcda6ff04d
Jason Favrod <mail@jasonfavrod.com>
2021-01-03 19:25:50 -1000

    Added SQLite3Gateway

---
+++ src/gateways/baseClasses/SQLiteGateway.ts
@@ -0,0 +1,33 @@
+import SQLite3, { Database } from 'better-sqlite3';
+
+import { ILogger } from "../../context/interfaces";
+import { IGatewayResponse, ITableDataGateway } from "../interfaces";
+import GatewayBase from './GatewayBase';
+
+export default abstract class SQLiteGateway extends GatewayBase implements ITableDataGateway {
+    public readonly table: string;
+
+    private dbFile: string;
+
+    constructor(dbFile: string, table: string, logger?: ILogger) {
+        super(logger);
+        this.dbFile = dbFile;
+        this.table = table;
+        this.logger = logger || console as ILogger;
+
+        this.getConnection = this.getConnection.bind(this);
+        this.delete = this.delete.bind(this);
+        this.find = this.find.bind(this);
+        this.insert = this.insert.bind(this);
+        this.update = this.update.bind(this);
+    }
+
+    public abstract delete(id: number): Promise<IGatewayResponse>;
+    public abstract find(query?: any): Promise<IGatewayResponse>;
+    public abstract insert(record: any): Promise<IGatewayResponse>;
+    public abstract update(record: any): Promise<IGatewayResponse>;
+
+    protected getConnection(): Database {
+        return new SQLite3(this.dbFile);
+    }
+}
---
+++ src/gateways/baseClasses/index.ts
@@ -0,0 +1,4 @@
+export { default as FirestoreGateway } from './FirestoreGateway';
+export { default as GatewayBase } from './GatewayBase';
+export { default as PostgresGateway } from './PostgresGateway';
+export { default as SQLiteGateway } from './SQLiteGateway';
